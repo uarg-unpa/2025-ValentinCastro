12f2ee216a174afa0f463689df3a9c1aef6a84c8
Valentín Peñaloza <valentinpenalozacastro@gmail.com>
2025-08-26 21:20:26 -0300

    update

--- tp1/eje1.java
+++ tp1/eje1.java
@@ -1,15 +1,21 @@
-package tp1; 
+package tp1;
 public class eje1 {
     public static void main(String[] args) {
-        int valor1 = 5, valor2 = 3, resultadoEntero; 
+        int valor1 = 5, valor2 = 3, resultadoEntero;
         double decimal1 = 2.5, decimal2 = 2.0, resultadoDecimal;
 
         resultadoEntero = valor1 / valor2;
-        resultadoDecimal = valor1 / decimal1; 
+        resultadoDecimal = valor1 / decimal1;
         resultadoDecimal = valor1 * decimal2;
         resultadoEntero = valor1 * (int) decimal2;
         resultadoEntero = (int) (decimal1 * decimal2);
-        resultadoEntero = valor1 * (int) decimal2; 
-        valor2 = valor1++;
+        resultadoEntero = valor1 * (int) decimal2;
+
+        System.out.println("El resultado es:" + (resultadoEntero = valor1 / valor2));
+        System.out.println("El resultado es:" + (resultadoDecimal = valor1 / decimal1));
+        System.out.println("El resultado es:" + (resultadoDecimal = valor1 * decimal2));
+        System.out.println("El resultado es:" + (resultadoEntero = valor1 * (int) decimal2));
+        System.out.println("El resultado es:" + (resultadoEntero = (int) (valor1 * decimal2)));
+        System.out.println("El resultado es:" + (resultadoEntero = valor1 * (int) decimal2));
     }
 }
