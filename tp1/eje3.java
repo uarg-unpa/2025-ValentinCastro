7222df655e744c024a44dc31c5f347686211ab1a
Valentín Peñaloza <valentinpenalozacastro@gmail.com>
2025-08-30 18:44:17 -0300

    update

--- tp1/eje3.java
+++ tp1/eje3.java
@@ -22,6 +22,6 @@ public class eje3 {
             System.out.println("valor de A: " + valorA);
             System.out.println("valor de B: " + valorB);
             System.out.println("valor de C: " + valorC);
-
+            scanner.close();
     }
 }
