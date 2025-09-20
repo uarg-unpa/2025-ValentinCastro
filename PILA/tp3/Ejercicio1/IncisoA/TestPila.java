ea30df80b8a2426dc4964971e80c26cd12ea6881
Valentín Peñaloza <valentinpenalozacastro@gmail.com>
2025-09-20 20:04:47 -0300

    update

---
+++ PILA/tp3/Ejercicio1/IncisoA/TestPila.java
@@ -0,0 +1,24 @@
+package PILA.tp3.Ejercicio1.IncisoA;
+
+public class TestPila {
+    public static void main(String[] args) {
+        
+        int X=3;
+        int Y=5;
+    int Z=2;
+    Pila P=new Pila();
+    P.meter(X);
+    P.meter(4);
+    X=P.sacar();
+    P.meter(Y);
+    P.meter(3);
+    P.meter(Z);
+    X=P.sacar();
+    P.meter(2);
+    P.meter(X);
+    while(!P.estaVacia()) {
+        X=P.sacar();
+        System.out.println(X);
+        }
+    }
+}
