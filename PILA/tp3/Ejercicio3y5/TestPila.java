516e21551bd59c3813860412ee00da4bc781d1f7
Valentín Peñaloza <valentinpenalozacastro@gmail.com>
2025-09-26 09:04:20 -0300

    update

--- PILA/tp3/Ejercicio3y5/TestPila.java
+++ PILA/tp3/Ejercicio3y5/TestPila.java
@@ -5,8 +5,11 @@ public class TestPila {
     public static void main(String[] args) {
         Pila p = new Pila ();
         String palabra = "programacion";
-        for (int i = palabra.length() -1; i >= 0 ; i-- )
-        System.out.print(palabra.charAt(i));
+        for (int i = palabra.length() -1; i >= 0 ; i-- ){
+        if (!p.estaVacia()) {
+        p.meter(palabra.charAt(i));
+        }
+        }
 
         if (p.estaVacia()){
             System.out.println("La pila esta vacia");
