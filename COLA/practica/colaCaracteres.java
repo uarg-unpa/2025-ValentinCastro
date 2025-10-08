4dda3f8110138fe25ee7ac6a6c48e58c769a9b27
Valentín Peñaloza <valentinpenalozacastro@gmail.com>
2025-10-08 13:12:10 -0300

    update

---
+++ COLA/practica/colaCaracteres.java
@@ -0,0 +1,44 @@
+package COLA.practica;
+
+public class colaCaracteres {
+        private char[] elementos;
+    private int frente;
+    private final int maxCola = 10;
+    private int fin;
+
+    public colaCaracteres(){
+        elementos = new char [maxCola];
+        frente = 0;
+        fin = 0;
+    }
+
+    public boolean estaVacia(){
+        return frente == fin;
+    }
+
+    public boolean estaLlena(){
+        return fin == maxCola;
+    }
+
+    public void insertar(char elem){
+        elementos [fin] = elem;
+
+        fin++;
+    }
+
+    public char borrar(){
+        char aux = elementos[frente];
+
+        for (int i = 0; i <  fin - 1; i++){
+            elementos[i] = elementos [i+1];
+        }
+        fin--;
+
+        return aux;
+
+    }
+
+    public char peek (){
+        return elementos [frente];
+    }
+}
